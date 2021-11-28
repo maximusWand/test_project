76fde2e3a9aa0943d82fec9a62ed225fa5b38ccf
maximusWand <maximus_wand@mail.ru>
2021-11-28 21:04:34 +0300

    Удаление ddd

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
 
 
-let f = document.querySelector('#h_1');
+let f = document.querySelector('#tab1');
+
+//f.addEventListener('mouseover', ddd);
+f.addEventListener("click", ddd);
+f.addEventListener("wheel", ddd);
+
+
+let d = document.querySelector('#h_1');
+d.addEventListener('mouseover', ddd);
 
 console.log(f);
 
 // The function is incomprehensible for what
 function ddd() {
-
-   console.log("sdds");
+   console.log(this.nodeName);
 }
-
